fb65a814215a8c2845233038714f16b23402f1dc
praveeng9632 <121174464+praveeng9632@users.noreply.github.com>
2023-06-26 00:39:50 +0530

    Update items.js

--- medilab-node-items-app-service/node-items-app-service/routes/items.js
+++ medilab-node-items-app-service/node-items-app-service/routes/items.js
@@ -11,6 +11,8 @@ const data = [
     { id: 5, title: 'Have some dinner', order: 5, completed: false, createdOn: new Date() },
     { id: 6, title: 'Have a cup of Coffe', order: 6, completed: false, createdOn: new Date() },
     { id: 7, title: 'Have a plate of Meal', order: 7, completed: false, createdOn: new Date() },
+    { id: 8, title: 'Have a briyani', order: 8, completed: false, createdOn: new Date() },
+    { id: 9, title: 'Have a plate of chicken', order: 8, completed: false, createdOn: new Date() },
 ];
 
 // READ
